ccb33c47010406357f5092031ef996094dd8dd00
ei_ei_kyaw_97 <39520950+EiEiKyaw@users.noreply.github.com>
2024-08-07 16:43:21 +0630

    update the codes for building war file

--- src/main/java/org/akee/testing/employee_mgnt/EmployeeMgntApplication.java
+++ src/main/java/org/akee/testing/employee_mgnt/EmployeeMgntApplication.java
@@ -3,14 +3,21 @@ package org.akee.testing.employee_mgnt;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class EmployeeMgntApplication {
+public class EmployeeMgntApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeMgntApplication.class, args);
 	}
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(EmployeeMgntApplication.class);
+    }
 
     @Bean
     ModelMapper modelMapper() {
